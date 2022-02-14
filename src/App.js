7fb97ec33af00117109602533fb452024d8e548f
Fahim Faisal <fahim.faisal.abir@gmail.com>
2022-02-14 23:49:18 +0600

    Project Initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import './tailwind.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="flex h-screen w-full flex-row">
+            <div className="basis-1/5 bg-[#FBFBFB]">GG</div>
+            <div className="basis-1/5 bg-white">GG</div>
+            <div className="divider divider-vertical" />
+            <div className="basis-3/5 bg-white">GG</div>
+        </div>
+    );
 }
 
 export default App;

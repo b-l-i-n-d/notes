9a1be4131750a366b06ed98ca25bd9c49fd28510
Fahim Faisal <fahim.faisal.abir@gmail.com>
2022-08-11 18:13:22 +0600

    Made suitable for heroku

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-underscore-dangle */
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
-import bodyParser from "body-parser";
-import cors from "cors";
-import "dotenv/config";
-import express from "express";
-import mongoose from "mongoose";
-import notesRoutes from "./routes/notes.js";
-import userRoutes from "./routes/users.js";
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import 'dotenv/config.js';
+import express from 'express';
+import mongoose from 'mongoose';
+import notesRoutes from './routes/notes.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
 app.use(
     bodyParser.urlencoded({
-        limit: "50mb",
+        limit: '50mb',
         extended: true,
         parameterLimit: 50000,
     })
@@ -19,11 +19,15 @@ app.use(
 app.use(cors());
 app.use(express.json());
 
-app.use("/notes", notesRoutes);
-app.use("/users", userRoutes);
+app.use('/', (req, res) => {
+    res.send('Afternotes API');
+});
+
+app.use('/notes', notesRoutes);
+app.use('/users', userRoutes);
 
 const CONNECTION_URL = process.env.DB_URL;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 mongoose
     .connect(CONNECTION_URL, {
@@ -31,8 +35,6 @@ mongoose
         useUnifiedTopology: true,
     })
     .then(() =>
-        app.listen(PORT, () =>
-            console.log(`Server Running on Port: http://localhost:${PORT}`)
-        )
+        app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`))
     )
     .catch((error) => console.log(`${error} did not connect`));

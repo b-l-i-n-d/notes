5f4024cece2b70afdc2a47b868849f01b06a3991
Fahim Faisal <fahim.faisal.abir@gmail.com>
2022-08-21 18:48:20 +0600

    Added folders api endpoints

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 import User from '../models/user.js';
 
 export const login = async (req, res) => {
@@ -55,5 +56,133 @@ export const signup = async (req, res) => {
         return res.status(200).json({ result, token });
     } catch (error) {
         return res.status(500).json({ message: 'Something went wrong.' });
+    }
+};
+
+export const addFolder = async (req, res) => {
+    const folder = req.body;
+
+    try {
+        await User.updateOne(
+            {
+                _id: req.userId,
+            },
+            {
+                $push: {
+                    folders: folder,
+                },
+            }
+        );
+        return res.status(200).json({
+            message: 'Folder created',
+        });
+    } catch (error) {
+        return res.status(404).json({ message: error.message });
+    }
+};
+
+export const updateFolder = async (req, res) => {
+    const { name } = req.body;
+    const { id: _id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).json({ message: 'No folder it that ID.' });
+    }
+
+    User.findOne(
+        {
+            _id: req.userId,
+            folders: {
+                $elemMatch: {
+                    _id,
+                },
+            },
+        },
+        {
+            'folders.$': 1,
+        },
+        (err, system) => {
+            if (err) {
+                return res.status(400).json({ message: err.message });
+            }
+            if (system === null) {
+                return res.status(404).json({ message: 'No folder it that ID' });
+            }
+            return system;
+        }
+    );
+
+    try {
+        await User.updateOne(
+            {
+                _id: req.userId,
+            },
+            {
+                $set: {
+                    'folders.$[element].name': name,
+                },
+            },
+            {
+                arrayFilters: [{ 'element._id': mongoose.Types.ObjectId(_id) }],
+            }
+        );
+
+        return res.status(200).json({
+            message: 'Folder updated',
+        });
+    } catch (error) {
+        return res.status(404).json({ message: error.message });
+    }
+};
+
+export const deleteFolder = async (req, res) => {
+    const { id: _id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send('No folder it that ID.');
+    }
+
+    User.findOne(
+        {
+            _id: req.userId,
+            folders: {
+                $elemMatch: {
+                    _id,
+                },
+            },
+        },
+        {
+            'folders.$': 1,
+        },
+        (err, system) => {
+            if (err) {
+                return res.status(400).json({ message: err.message });
+            }
+            if (system === null) {
+                return res.status(404).json({ message: 'No folder it that ID' });
+            }
+            return system;
+        }
+    );
+
+    try {
+        await User.updateOne(
+            {
+                _id: req.userId,
+            },
+            {
+                $pull: {
+                    folders: {
+                        _id,
+                    },
+                },
+            }
+        );
+
+        return res.status(200).json({
+            message: 'Folder deleted',
+        });
+    } catch (error) {
+        return res.status(409).json({ message: error.message });
     }
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { login, signup } from '../controllers/users.js';
+import { addFolder, deleteFolder, login, signup, updateFolder } from '../controllers/users.js';
+import auth from '../middleware/auth.js';
 import { userValidationRules, validate } from '../middleware/validator.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/signup', userValidationRules(), validate, signup);
+router.patch('/folders', auth, addFolder);
+router.patch('/folders/:id', auth, updateFolder);
+router.patch('/folders/:id/delete', auth, deleteFolder);
 
 export default router;
